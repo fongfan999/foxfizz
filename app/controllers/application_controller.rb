bde01857d02d11d6bafc16f2755082e222b876a8
Phong Phan <fongfan999@gmail.com>
2017-12-03 16:28:50 +0700

    Add sessions helper

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
+  include SessionsHelper
+
   protect_from_forgery with: :exception
+
+  protected
+
+  def authenticate_user!
+    unless user_signed_in?
+      store_user_location!
+      redirect_to root_path, alert: I18n.t('sessions.failure.unauthenticated')
+    end
+  end
 end
